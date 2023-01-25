1173dbf1b336b6b1a7b20979f1ca395e11c7c0e6
Roberto Casadei <robyonrails@gmail.com>
2023-01-25 15:42:32 +0100

    fix on texfitler split and flatmap

--- entrypoint.rb
+++ entrypoint.rb
@@ -23,8 +23,7 @@ end
 initial_directory = File.expand_path('.') + '/'
 puts "Working from #{initial_directory}"
 puts "Using filter '#{texfilter}'"
-tex_filters = texfilter.split(/ /)
-tex_filters = texfilter.flat_map { |x| x.split(/,/) }.map { |f| initial_directory + f }
+tex_filters = texfilter.split(/ /).flat_map { |x| x.split(/,/) }.map { |f| initial_directory + f }
 puts "Searching #{tex_filters}"
 tex_files = Dir[*tex_filters]
 puts "Found these tex files: #{tex_files}" # if verbose
